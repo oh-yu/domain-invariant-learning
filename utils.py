2f8e97a95c24e447c32e67c7582907756a8e60b5
hisasu <perusyanekolove0320@gmail.com>
2023-02-20 09:03:32 +0900

    encapsulated # Prepare Data

--- utils.py
+++ utils.py
@@ -16,3 +16,33 @@ def get_source_target():
     x1_grid, x2_grid = np.meshgrid(np.linspace(x1_min-0.1, x1_max+0.1, 100), np.linspace(x2_min-0.1, x2_max+0.1, 100))
     x_grid = np.stack([x1_grid.reshape(-1), x2_grid.reshape(-1)])
     return Xs, Xs_rotated, ys, ys_rotated, x_grid, x1_grid, x2_grid
+
+
+def get_loader(source_X, target_X, source_y_task, target_y_task):
+    # 1. Create y_domain
+    source_y_domain = np.zeros_like(source_y_task).reshape(-1, 1)
+    source_y_task = source_y_task.reshape(-1, 1)
+    source_Y = np.concatenate([source_y_task, source_y_domain], axis=1)
+    target_y_domain = np.ones_like(target_y_task)
+
+    # 2. Instantiate torch.tensor
+    source_X = torch.tensor(source_X, dtype=torch.float32)
+    source_Y = torch.tensor(source_Y, dtype=torch.float32)
+    target_X = torch.tensor(target_X, dtype=torch.float32)
+    target_y_domain = torch.tensor(target_y_domain, dtype=torch.float32)
+    target_y_task = torch.tensor(target_y_task, dtype=torch.float32)
+    
+    # 3. To GPU
+    source_X = source_X.to(device)
+    source_Y = source_Y.to(device)
+    target_X = target_X.to(device)
+    target_y_domain = target_y_domain.to(device)
+    target_y_task = target_y_task.to(device)
+    
+    # 4. Instantiate DataLoader
+    source_ds = TensorDataset(source_X, source_Y)
+    target_ds = TensorDataset(target_X, target_y_domain)
+    source_loader = DataLoader(source_ds, batch_size=34, shuffle=True)
+    target_loader = DataLoader(target_ds, batch_size=34, shuffle=True)
+    
+    return source_loader, target_loader
