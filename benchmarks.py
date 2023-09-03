db7ec9eee2bc440ac7d79785622acb605e740f24
hisasu <perusyanekolove0320@gmail.com>
2023-09-03 15:01:15 +0900

    in benchmarks.py, 60m to 30m, change correspoding to def fit()

--- benchmarks.py
+++ benchmarks.py
@@ -1,12 +1,13 @@
 import pandas as pd
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
 import torch
 from torch import nn
 from torch import optim
 
 import utils
+
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 HOUSEHOLD_IDXS = [1, 2, 3]
 
@@ -26,8 +27,8 @@ def main(source_idx, target_idx, winter_idx, summer_idx):
     scaler = preprocessing.StandardScaler()
     train_source_X = scaler.fit_transform(train_source_X)
     target_X = scaler.fit_transform(target_X)
-    train_source_X, train_source_y_task = utils.apply_sliding_window(train_source_X, train_source_y_task, filter_len=3)
-    target_X, target_y_task = utils.apply_sliding_window(target_X, target_y_task, filter_len=3)
+    train_source_X, train_source_y_task = utils.apply_sliding_window(train_source_X, train_source_y_task, filter_len=6)
+    target_X, target_y_task = utils.apply_sliding_window(target_X, target_y_task, filter_len=6)
     train_target_X, test_target_X, train_target_y_task, test_target_y_task = train_test_split(target_X, target_y_task, test_size=0.5, shuffle=False)
     source_loader, target_loader, _, _, _, _ = utils.get_loader(train_source_X, train_target_X, train_source_y_task, train_target_y_task, shuffle=True)
     # TODO: Update utils.get_loader's docstring
@@ -43,8 +44,7 @@ def main(source_idx, target_idx, winter_idx, summer_idx):
     num_domains = 1
     num_classes = 1
 
-    feature_extractor = utils.ManyToOneRNN(input_size=train_source_X.shape[2], hidden_size=hidden_size, num_layers=3).to(device)
-    # feature_extractor = utils.Conv1d(input_size=train_source_X.shape[2]).to(device)
+    feature_extractor = utils.Conv1d(input_size=train_source_X.shape[2]).to(device)
     domain_classifier = utils.Decoder(input_size=hidden_size, output_size=num_domains).to(device)
     task_classifier = utils.Decoder(input_size=hidden_size, output_size=num_classes).to(device)
 
@@ -55,10 +55,10 @@ def main(source_idx, target_idx, winter_idx, summer_idx):
     domain_optimizer = optim.Adam(domain_classifier.parameters(), lr=learning_rate)
     task_optimizer = optim.Adam(task_classifier.parameters(), lr=learning_rate)
 
-    num_epochs = 275
-    feature_extractor, task_classifier = utils.fit(source_loader, target_loader, test_target_X, test_target_y_task,
-                                                   feature_extractor, domain_classifier, task_classifier, criterion,
-                                                   feature_optimizer, domain_optimizer, task_optimizer, num_epochs=num_epochs, is_timeseries=False)
+    num_epochs = 300
+    feature_extractor, task_classifier, _ = utils.fit(source_loader, target_loader, test_target_X, test_target_y_task,
+                                                      feature_extractor, domain_classifier, task_classifier, criterion,
+                                                      feature_optimizer, domain_optimizer, task_optimizer, num_epochs=num_epochs, is_timeseries=False)
     
     pred_y_task = task_classifier(feature_extractor(test_target_X))
     pred_y_task = torch.sigmoid(pred_y_task).reshape(-1)
