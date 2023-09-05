9616fb7c10b99d803abcabc36456036f779f7640
hisasu <perusyanekolove0320@gmail.com>
2023-09-05 09:27:31 +0900

    implement line 41-55 using IsihDanns

--- cschDA.py
+++ cschDA.py
@@ -6,6 +6,7 @@ from torch import nn
 from torch import optim
 
 import utils
+from isih_DA import IsihDanns
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 HOUSEHOLD_IDXS = [1, 2, 3]
 
@@ -15,10 +16,8 @@ def main(source_idx, target_idx, winter_idx, summer_idx):
     ## Prepare Data
     train_source_X = pd.read_csv(f"./deep_occupancy_detection/data/{source_idx}_X_train.csv")
     target_X = pd.read_csv(f"./deep_occupancy_detection/data/{target_idx}_X_train.csv")
-
     train_source_y_task = pd.read_csv(f"./deep_occupancy_detection/data/{source_idx}_Y_train.csv")[train_source_X.Season==winter_idx].values.reshape(-1)
     target_y_task = pd.read_csv(f"./deep_occupancy_detection/data/{target_idx}_Y_train.csv")[target_X.Season==winter_idx].values.reshape(-1)
-
     train_source_X = train_source_X[train_source_X.Season==winter_idx]
     target_X = target_X[target_X.Season==winter_idx]
 
@@ -28,44 +27,21 @@ def main(source_idx, target_idx, winter_idx, summer_idx):
 
     train_source_X, train_source_y_task = utils.apply_sliding_window(train_source_X, train_source_y_task, filter_len=6)
     target_X, target_y_task = utils.apply_sliding_window(target_X, target_y_task, filter_len=6)
-    train_target_X, test_target_X, train_target_y_task, test_target_y_task = target_X, target_X, target_y_task, target_y_task
 
+    train_target_X, test_target_X, train_target_y_task, test_target_y_task = target_X, target_X, target_y_task, target_y_task
     source_loader, target_loader, _, _, _, _ = utils.get_loader(train_source_X, train_target_X, train_source_y_task, train_target_y_task, shuffle=True)
-    # TODO: Update utils.get_loader's docstring
 
     test_target_X = torch.tensor(test_target_X, dtype=torch.float32)
     test_target_y_task = torch.tensor(test_target_y_task, dtype=torch.float32)
     test_target_X = test_target_X.to(device)
     test_target_y_task = test_target_y_task.to(device)
 
-    hidden_size = 128
-    num_domains = 1
-    num_classes = 1
+    ## isih-DA fit, predict for 1st dimension
+    isih_dann = IsihDanns(input_size=train_source_X.shape[2], hidden_size=128, lr_dim1=0.0001, lr_dim2=0.00005, num_epochs_dim1=200)
+    isih_dann.fit_1st_dim(source_loader, target_loader, test_target_X, test_target_y_task)
+    pred_y_task = isih_dann.predict(test_target_X, is_1st_dim=True)
 
-    ## Instantiate Feature Extractor, Domain Classifier, Task Classifier
-    feature_extractor = utils.Conv1d(input_size=train_source_X.shape[2]).to(device)
-    domain_classifier = utils.Decoder(input_size=hidden_size, output_size=num_domains).to(device)
-    task_classifier = utils.Decoder(input_size=hidden_size, output_size=num_classes).to(device)
-
-    learning_rate = 0.0001
-
-    criterion = nn.BCELoss()
-    feature_optimizer = optim.Adam(feature_extractor.parameters(), lr=learning_rate)
-    domain_optimizer = optim.Adam(domain_classifier.parameters(), lr=learning_rate)
-    task_optimizer = optim.Adam(task_classifier.parameters(), lr=learning_rate)
-
-    ## Domain Invariant Learning
-    num_epochs = 200
-    feature_extractor, task_classifier, _ = utils.fit(source_loader, target_loader, test_target_X, test_target_y_task,
-                                                      feature_extractor, domain_classifier, task_classifier, criterion,
-                                                      feature_optimizer, domain_optimizer, task_optimizer, num_epochs=num_epochs, is_timeseries=False)
-    
-
-    # Algo2. Psuedo Labeling
-    pred_y_task = task_classifier(feature_extractor(test_target_X))
-    pred_y_task = torch.sigmoid(pred_y_task).reshape(-1)
-
-    # Algo3. Cross Season DA
+    # Algo2. Cross Season DA
     ## Prepare Data
     train_source_X = target_X
     train_source_y_task = pred_y_task.cpu().detach().numpy()
