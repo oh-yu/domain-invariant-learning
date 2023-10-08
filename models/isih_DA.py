eaf7fac03466febf63546899227f95fa270b8494
hisasu <perusyanekolove0320@gmail.com>
2023-10-08 15:38:25 +0900

    typehint docstring isih_DA

--- models/isih_DA.py
+++ models/isih_DA.py
@@ -8,7 +8,10 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class IsihDanns:
-    def __init__(self, input_size, hidden_size, lr_dim1, lr_dim2, num_epochs_dim1, num_epochs_dim2):
+    """
+    TODO: Attach ICASSP2024 REF
+    """
+    def __init__(self, input_size: int, hidden_size: int, lr_dim1: float, lr_dim2: float, num_epochs_dim1: int, num_epochs_dim2: int):
         self.feature_extractor = utils.Conv1d(input_size=input_size).to(DEVICE)
         self.domain_classifier_dim1 = utils.Decoder(input_size=hidden_size, output_size=1).to(DEVICE)
         self.task_classifier_dim1 = utils.Decoder(input_size=hidden_size, output_size=1).to(DEVICE)
@@ -25,21 +28,21 @@ class IsihDanns:
         self.task_optimizer_dim2 = optim.Adam(self.task_classifier_dim2.parameters(), lr=lr_dim2)
         self.num_epochs_dim2 = num_epochs_dim2
 
-    def fit_1st_dim(self, source_loader, target_loader, test_target_X, test_target_y_task):
+    def fit_1st_dim(self, source_loader, target_loader, test_target_X: torch.Tensor, test_target_y_task: torch.Tensor):
         self.feature_extractor, self.task_classifier_dim1, _ = utils.fit(
             source_loader, target_loader, test_target_X, test_target_y_task,
             self.feature_extractor, self.domain_classifier_dim1, self.task_classifier_dim1, self.criterion,
             self.feature_optimizer_dim1, self.domain_optimizer_dim1, self.task_optimizer_dim1, num_epochs=self.num_epochs_dim1
         )
 
-    def fit_2nd_dim(self, source_loader, target_loader, test_target_X, test_target_y_task):
+    def fit_2nd_dim(self, source_loader, target_loader, test_target_X: torch.Tensor, test_target_y_task: torch.Tensor):
         self.feature_extractor, self.task_classifier_dim2, _ = utils.fit(
             source_loader, target_loader, test_target_X, test_target_y_task,
             self.feature_extractor, self.domain_classifier_dim2, self.task_classifier_dim2, self.criterion,
             self.feature_optimizer_dim2, self.domain_optimizer_dim2, self.task_optimizer_dim2, num_epochs=self.num_epochs_dim2, is_psuedo_weights=True
         )
 
-    def predict(self, X, is_1st_dim: bool) -> torch.Tensor:
+    def predict(self, X: torch.Tensor, is_1st_dim: bool) -> torch.Tensor:
         if is_1st_dim:
             out = self.task_classifier_dim1(self.feature_extractor(X))
             out = torch.sigmoid(out).reshape(-1)
