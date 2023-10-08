5168e9e2a23fe4507f227924698944adde12f496
hisasu <perusyanekolove0320@gmail.com>
2023-10-08 15:30:19 +0900

    type hint, docstring, set params

--- models/codats.py
+++ models/codats.py
@@ -8,10 +8,13 @@ DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class Codats:
-    def __init__(self, input_size, hidden_size, lr, num_epochs):
+    """
+    CoDATS model https://arxiv.org/abs/2005.10996
+    """
+    def __init__(self, input_size: int, hidden_size: int, lr: float, num_epochs: int, num_domains: int = 1, num_classes: int = 1):
         self.feature_extractor = utils.Conv1d(input_size=input_size).to(DEVICE)
-        self.domain_classifier = utils.Decoder(input_size=hidden_size, output_size=1).to(DEVICE)
-        self.task_classifier = utils.Decoder(input_size=hidden_size, output_size=1).to(DEVICE)
+        self.domain_classifier = utils.Decoder(input_size=hidden_size, output_size=num_domains).to(DEVICE)
+        self.task_classifier = utils.Decoder(input_size=hidden_size, output_size=num_classes).to(DEVICE)
         self.criterion = nn.BCELoss()
 
         self.feature_optimizer = optim.Adam(self.feature_extractor.parameters(), lr=lr)
@@ -19,7 +22,8 @@ class Codats:
         self.task_optimizer = optim.Adam(self.task_classifier.parameters(), lr=lr)
         self.num_epochs = num_epochs
 
-    def fit(self, source_loader, target_loader, test_target_X, test_target_y_task):
+    def fit(self, source_loader: torch.utils.data.dataloader.DataLoader, target_loader: torch.utils.data.dataloader.DataLoader,
+            test_target_X: torch.Tensor, test_target_y_task: torch.Tensor):
         self.feature_extractor, self.task_classifier, _ = utils.fit(
             source_loader, target_loader, test_target_X, test_target_y_task,
             self.feature_extractor, self.domain_classifier, self.task_classifier, self.criterion,
@@ -27,7 +31,7 @@ class Codats:
             num_epochs=self.num_epochs
         )
 
-    def predict(self, x):
+    def predict(self, x: torch.Tensor):
         pred_y_task = self.task_classifier(self.feature_extractor(x))
         pred_y_task = torch.sigmoid(pred_y_task).reshape(-1)
         return pred_y_task
