17cddcbdd69865caed0ab57e2d5e409416f12a11
hisasu <perusyanekolove0320@gmail.com>
2024-03-31 10:21:12 +0900

    revise logic for isihDA_house

--- ecodataset_synthetic/experiment.py
+++ ecodataset_synthetic/experiment.py
@@ -22,7 +22,7 @@ class CoDATS_F_C(nn.Module):
     def forward(self, x):
         return self.decoder(self.conv1d(x))
 
-def isih_da(source_idx=2, season_idx=0, n_splits:int=5):
+def isih_da(source_idx=2, season_idx=0, n_splits:int=5, is_kfold_eval: bool=False):
     train_source_X = pd.read_csv(f"./domain-invariant-learning/deep_occupancy_detection/data/{source_idx}_X_train.csv")
     train_source_y_task = pd.read_csv(f"./domain-invariant-learning/deep_occupancy_detection/data/{source_idx}_Y_train.csv")[train_source_X.Season == season_idx]
     train_source_X = train_source_X[train_source_X.Season == season_idx]
@@ -47,7 +47,8 @@ def isih_da(source_idx=2, season_idx=0, n_splits:int=5):
 
     scaler = preprocessing.StandardScaler()
     train_source_X = scaler.fit_transform(train_source_X)
-    target_X = scaler.fit_transform(target_X)
+    scaler.fit(target_X)
+    target_X = scaler.transform(target_X)
 
     train_source_X, train_source_y_task = utils.apply_sliding_window(train_source_X, train_source_y_task, filter_len=6)
     target_X, target_y_task = utils.apply_sliding_window(target_X, target_y_task, filter_len=6)
@@ -70,13 +71,30 @@ def isih_da(source_idx=2, season_idx=0, n_splits:int=5):
     target_X = target_prime_X
     target_y_task = target_prime_y_task
 
-    target_X = scaler.fit_transform(target_X)
+    target_X = scaler.transform(target_X)
     target_X, target_y_task = utils.apply_sliding_window(target_X, target_y_task, filter_len=6)
+    if is_kfold_eval:
+        kfold = KFold(n_splits=n_splits, shuffle=False)
+        accs = []
+        for train_idx, test_idx in kfold.split(target_X):
+            train_target_X, test_target_X, train_target_y_task, test_target_y_task = target_X[train_idx], target_X[test_idx], target_y_task[train_idx], target_y_task[test_idx]
+            source_loader, target_loader, _, _, _, _ = utils.get_loader(train_source_X, train_target_X, train_source_y_task, train_target_y_task, shuffle=True)
 
-    kfold = KFold(n_splits=n_splits, shuffle=False)
-    accs = []
-    for train_idx, test_idx in kfold.split(target_X):
-        train_target_X, test_target_X, train_target_y_task, test_target_y_task = target_X[train_idx], target_X[test_idx], target_y_task[train_idx], target_y_task[test_idx]
+            test_target_X = torch.tensor(test_target_X, dtype=torch.float32)
+            test_target_y_task = torch.tensor(test_target_y_task, dtype=torch.float32)
+            test_target_X = test_target_X.to(DEVICE)
+            test_target_y_task = test_target_y_task.to(DEVICE)
+            ## isih-DA fit, predict for 2nd dimension
+            isih_dann.fit_2nd_dim(source_loader, target_loader, test_target_X, test_target_y_task)
+            pred_y_task = isih_dann.predict(test_target_X, is_1st_dim=False)
+
+            # Algo3. Evaluation
+            pred_y_task = pred_y_task > 0.5
+            acc = sum(pred_y_task == test_target_y_task) / test_target_y_task.shape[0]
+            accs.append(acc.item())
+        return sum(accs)/n_splits
+    else:
+        train_target_X, test_target_X, train_target_y_task, test_target_y_task = train_test_split(target_X, target_y_task, test_size=0.5, shuffle=False)
         source_loader, target_loader, _, _, _, _ = utils.get_loader(train_source_X, train_target_X, train_source_y_task, train_target_y_task, shuffle=True)
 
         test_target_X = torch.tensor(test_target_X, dtype=torch.float32)
@@ -90,8 +108,7 @@ def isih_da(source_idx=2, season_idx=0, n_splits:int=5):
         # Algo3. Evaluation
         pred_y_task = pred_y_task > 0.5
         acc = sum(pred_y_task == test_target_y_task) / test_target_y_task.shape[0]
-        accs.append(acc.item())
-    return sum(accs)/n_splits
+        return acc.item()
     
 def codats(source_idx=2, season_idx=0, n_splits:int=5):
     train_source_X = pd.read_csv(f"./domain-invariant-learning/deep_occupancy_detection/data/{source_idx}_X_train.csv")
