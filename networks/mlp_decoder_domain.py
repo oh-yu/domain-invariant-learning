f8e746f16c7d6c3076d517ee302311762954febc
hisasu <perusyanekolove0320@gmail.com>
2024-04-24 15:49:33 +0900

    user Domain dec in make_moons experiment

--- networks/mlp_decoder_domain.py
+++ networks/mlp_decoder_domain.py
@@ -4,12 +4,12 @@ from torch import nn
 
 
 class DomainDecoder(nn.Module):
-    def __init__(self, input_size, output_size, fc1_size=500, fc2_size=500):
+    def __init__(self, input_size, output_size, fc1_size=500, fc2_size=500, dropout_ratio=0.3):
         super().__init__()
         self.fc1 = nn.Linear(input_size, fc1_size)
-        self.dropout1 = nn.Dropout(0.3)
+        self.dropout1 = nn.Dropout(dropout_ratio)
         self.fc2 = nn.Linear(fc1_size, fc2_size)
-        self.dropout2 = nn.Dropout(0.3)
+        self.dropout2 = nn.Dropout(dropout_ratio)
         self.fc3 = nn.Linear(fc2_size, output_size)
         self.output_size = output_size
 
