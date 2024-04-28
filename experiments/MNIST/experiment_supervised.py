0032d97a46870eb97d976490cfa15e2eac87aea0
hisasu <perusyanekolove0320@gmail.com>
2024-04-28 18:52:09 +0900

    if name == MNIST

--- experiments/MNIST/experiment_supervised.py
+++ experiments/MNIST/experiment_supervised.py
@@ -5,26 +5,43 @@ from torchvision import datasets, transforms
 from ...networks import Conv2d, DomainDecoder
 
 
-def get_mnist_data():
-    transform = transforms.ToTensor()
-    train_data = torchvision.datasets.SVHN(
-        './data/SVHN', 
-        split='train',
-        download=True,
-        transform=transform)
-    train_loader = DataLoader(train_data, batch_size=128, shuffle=True)
-    test_data = torchvision.datasets.SVHN(
-        "./data/SVHN",
-        split="test",
-        download=True,
-        transform=transform)
-    test_loader = DataLoader(test_data, batch_size=128, shuffle=False)
-    return train_loader, test_loader
+class Reshape(transforms.Transform):
+    def __call__(self, img):
+        return img[0].repeat(3, 1, 1).permute(1, 2, 0)
+
+def get_image_data_for_uda(name="MNIST"):
+    if name == "MNIST":
+        custom_transform = transforms.Compose([
+            Reshape(),
+            transforms.ToTensor()
+        ])
+        train_data = datasets.MNIST(root="./data/MNIST", train=True, download=True, transform=custom_transform)
+        train_loader = DataLoader(train_data, batch_size=128, shuffle=True)
+        return train_loader
+
+    elif name == "SVHN":
+        transform = transforms.ToTensor()
+        train_data = torchvision.datasets.SVHN(
+            './data/SVHN', 
+            split='train',
+            download=True,
+            transform=transform)
+        train_loader = DataLoader(train_data, batch_size=128, shuffle=True)
+        test_data = torchvision.datasets.SVHN(
+            "./data/SVHN",
+            split="test",
+            download=True,
+            transform=transform)
+        test_loader = DataLoader(test_data, batch_size=128, shuffle=False)
+        return train_loader, test_loader
+
+
+
 
 
 if __name__ == "__main__":
     # Load Data
-    train_loader, test_loader = get_mnist_data()
+    train_loader, test_loader = get_image_data_for_uda()
 
     # Model Init
     feature_extractor = Conv2d()
