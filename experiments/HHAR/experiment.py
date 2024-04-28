b08a5c6f71718d7668996bff5049d21f31264875
oh-yu <perusyanekolovve0320@gmail.com>
2024-04-28 11:56:54 +0900

    revise, stratify=y

--- experiments/HHAR/experiment.py
+++ experiments/HHAR/experiment.py
@@ -34,8 +34,8 @@ def get_data_for_uda(user, model, is_targer_prime: bool = False):
         X, y = utils.apply_sliding_window(df[["x_accele", "y_accele", "z_accele", "x_gyro", "y_gyro", "z_gyro"]].values, df["gt_accele"].values.reshape(-1), filter_len=128, is_overlap=False)
         return X, y
     else:
-        X, y = utils.apply_sliding_window(df[["x_accele", "y_accele", "z_accele", "x_gyro", "y_gyro", "z_gyro"]].values, df["gt_accele"].values, filter_len=128, is_overlap=False)
-        train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.5, shuffle=False, stratify=True)
+        X, y = utils.apply_sliding_window(df[["x_accele", "y_accele", "z_accele", "x_gyro", "y_gyro", "z_gyro"]].values, df["gt_accele"].values.reshape(-1), filter_len=128, is_overlap=False)
+        train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.5, shuffle=False, stratify=y)
         # Shuffle = False, since data has meaning of time series
         N, T, H = train_X.shape[2]
         train_X, test_X = train_X.reshape(T*T, H), test_X.reshape(T*H, H)
