b01fb9d7cc3dad0ebbec689aeb106e2f90c042e4
hisasu <perusyanekolove0320@gmail.com>
2024-04-28 17:50:08 +0900

    implemented experiment supervised

--- experiments/MNIST/experiment_supervised.py
+++ experiments/MNIST/experiment_supervised.py
@@ -1,3 +1,4 @@
+from torch import nn, optim
 from torch.utils.data import DataLoader
 import torchvision
 from torchvision import datasets, transforms
@@ -20,15 +21,23 @@ if __name__ == "__main__":
     test_loader = DataLoader(test_data, batch_size=128, shuffle=False)
     # Model Init
     feature_extractor = Conv2d()
-    task_classifier = DomainDecoder(input_size=100, output_size=10, fc2_size=50)
-    # TODO: check input_size
-    # TODO: init criterion, optimizer
+    task_classifier = DomainDecoder(input_size=1600, output_size=10, fc2_size=50)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(list(feature_extractor.parameters()) + list(task_classifier.parameters()), lr=1e-3)
 
     # Training
-    for X_batch, y_batch in train_loader:
-        out = feature_extractor(X_batch)
-        print(out.shape)
-
-
+    for _ in range(100):
+        for X_batch, y_batch in train_loader:
+            out = task_classifier.predict_proba(feature_extractor(X_batch))
+            loss = criterion(out, y_batch)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        if _ % 10:
+            print(f"Epoch {_}: Loss {loss}")
 
     # Evaluation
+    for X_batch, y_batch in test_loader:
+        out = task_classifier.predict(feature_extractor(X_batch))
+        acc = sum(out == y_batch) / len(y_batch)
+        print(acc)
