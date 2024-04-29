da8fd4d4f1f34f9891ed2b1d6a239d5ab3bf08d3
hisasu <perusyanekolove0320@gmail.com>
2024-04-29 12:18:59 +0900

    commentout

--- experiments/MNIST/experiment.py
+++ experiments/MNIST/experiment.py
@@ -107,6 +107,7 @@ if __name__ == "__main__":
     )
     target_X = torch.cat([X for X, _ in target_loader], dim=0)
     target_y_task = torch.cat([y for _, y in target_loader], dim=0)
+    # TODO: now this has domain label only
     target_X = torch.tensor(target_X, dtype=torch.float32).to(utils.DEVICE)
     target_y_task = torch.tensor(target_y_task, dtype=torch.long).to(utils.DEVICE)
     isih_dann.fit_1st_dim(source_loader, target_loader, target_X, target_y_task)
