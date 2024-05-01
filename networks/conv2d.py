97b888bfcd720fac36ae02cd6ab2ec9d0873fa76
oh-yu <perusyanekolovve0320@gmail.com>
2024-05-01 13:48:48 +0900

    resolved runtime error

--- networks/conv2d.py
+++ networks/conv2d.py
@@ -14,10 +14,11 @@ class Conv2d(nn.Module):
         # TODO: Understand nn.Conv2d, nn.MaxPool2d DOC
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
+        # TODO: Understand how this workaround is working
+        # https://discuss.pytorch.org/t/runtimeerror-one-of-the-variables-needed-for-gradient-computation-has-been-modified-by-an-inplace-operation-torch-floattensor-64-1-which-is-output-0-of-asstridedbackward0-is-at-version-3-expected-version-2-instead-hint-the-backtrace-further-a/171826/7
+        x = F.relu(nn.functional.conv2d(x, self.conv1.weight.clone(), self.conv1.bias, stride=1, padding=1))
         x = self.max_pool1(x)
-        x = F.relu(self.conv2(x))
+        x = F.relu(nn.functional.conv2d(x, self.conv2.weight.clone(), self.conv2.bias, stride=1, padding=1))
         x = self.max_pool2(x)
-        X = F.relu(self.conv3(x))
         N = x.shape[0]
         return x.reshape(N, -1)
