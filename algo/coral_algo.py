1c804b69db54cad662fac86a8da77877e44471da
hisasu <perusyanekolove0320@gmail.com>
2024-05-01 17:55:10 +0900

    implemented #0.

--- algo/coral_algo.py
+++ algo/coral_algo.py
@@ -1,4 +1,5 @@
-
+import torch
+from ..utils import utils
 
 def fit_coral(source_loader, target_loader, num_epochs, task_classifier, criterion, optimizer, is_psuedo_weights):
     for epoch in range(1, num_epochs.item() + 1):
@@ -7,6 +8,18 @@ def fit_coral(source_loader, target_loader, num_epochs, task_classifier, criteri
             source_loader, target_loader
         ):
             # 0. Data
+            if task_classifier.output_size == 1:
+                source_y_task_batch = source_Y_batch[:, utils.COL_IDX_TASK] > 0.5
+                source_y_task_batch = source_y_task_batch.to(torch.float32)
+            else:
+                if is_psuedo_weights:
+                    output_size = source_Y_batch[:, :-1].shape[1]
+                    source_y_task_batch = source_Y_batch[:, :output_size]
+                    source_y_task_batch = torch.argmax(source_y_task_batch, dim=1)
+                    source_y_task_batch = source_y_task_batch.to(torch.long)
+                else:
+                    source_y_task_batch = source_Y_batch[:, utils.COL_IDX_TASK]
+                    source_y_task_batch = source_y_task_batch.to(torch.long)
             # 1. Forward
             # 1.1 Task Loss
             # 1.2 CoRAL Loss
