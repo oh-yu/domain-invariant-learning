39b50243cab9bc87887bbfe0695df6fdab4d3fd3
hisasu <perusyanekolove0320@gmail.com>
2024-05-10 09:57:29 +0900

    no need > 0.5

--- experiments/ecodataset/experiment.py
+++ experiments/ecodataset/experiment.py
@@ -283,8 +283,6 @@ def codats(
         codats.fit(source_loader, target_loader, test_target_X, test_target_y_task)
         codats.set_eval()
         pred_y_task = codats.predict(test_target_X)
-
-        pred_y_task = pred_y_task > 0.5
         acc = sum(pred_y_task == test_target_y_task) / test_target_y_task.shape[0]
         accs.append(acc.item())
     return sum(accs) / num_repeats
