2bef78ef76c33f8dabae45d5c238b9b9715f6137
hisasu <perusyanekolove0320@gmail.com>
2024-05-13 10:29:04 +0900

    separate C config for ECOdataset or HHAR

--- networks/codats_fc.py
+++ networks/codats_fc.py
@@ -5,6 +5,7 @@ from .conv1d_three_layers import Conv1dThreeLayers
 from .conv1d_two_layers import Conv1dTwoLayers
 
 from .mlp_decoder_one_layer import OneLayerDecoder
+from .mlp_decoder_three_layers import ThreeLayersDecoder
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -15,9 +16,10 @@ class CoDATS_F_C(nn.Module):
         assert experiment in ["ECOdataset", "ECOdataset_synthetic", "HHAR"]
         if experiment in ["ECOdataset", "ECOdataset_synthetic"]:
             self.conv1d = Conv1dTwoLayers(input_size=input_size).to(DEVICE)
+            self.decoder = ThreeLayersDecoder(input_size=128, output_size=1, dropout_ratio=0, fc1_size=50, fc2_size=10).to(DEVICE)
         elif experiment == "HHAR":
             self.conv1d = Conv1dThreeLayers(input_size=input_size).to(DEVICE)
-        self.decoder = OneLayerDecoder(input_size=128, output_size=output_size).to(DEVICE)
+            self.decoder = OneLayerDecoder(input_size=128, output_size=output_size).to(DEVICE)
     def forward(self, x):
         return self.decoder(self.conv1d(x))
     
