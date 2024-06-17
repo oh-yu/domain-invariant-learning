76f619296ad8969d752ba48a9311f4fda64790f3
hisasu <perusyanekolove0320@gmail.com>
2024-06-17 11:02:07 +0900

    fix:should not use test_target_y_task for validate

--- networks/isih_DA.py
+++ networks/isih_DA.py
@@ -183,7 +183,7 @@ class IsihDanns:
         self.task_optimizer_dim1.param_groups[0].update(best_param)
         source_loader = DataLoader(source_ds, batch_size=self.batch_size, shuffle=True)
         target_loader = DataLoader(target_ds, batch_size=self.batch_size, shuffle=True)
-        self.fit_1st_dim(source_loader, target_loader, test_target_X, test_target_y_task)
+        self.fit_1st_dim(source_loader, target_loader, val_source_X, val_source_y_task)
         self.set_eval()
         pred_y_task = self.predict(test_target_X, is_1st_dim=True)
         acc = sum(pred_y_task == test_target_y_task) / test_target_y_task.shape[0]
