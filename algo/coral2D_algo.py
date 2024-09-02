559dbe84cdd91c4228d404e00464e237b2ecdce3
hisasu <perusyanekolove0320@gmail.com>
2024-09-02 11:45:18 +0900

    implemetend forward, backward, update params, validation

--- algo/coral2D_algo.py
+++ algo/coral2D_algo.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 
 from ..utils import utils
 from .algo_utils import EarlyStopping
-from .coral_algo import get_covariance_matrix, get_MSE
+from .coral_algo import get_covariance_matrix, get_MSE, plot_coral_loss
 
 def fit(data, network, **kwargs):
     # Args
@@ -17,24 +17,24 @@ def fit(data, network, **kwargs):
         data["target_prime_loader"],
     )
     target_prime_X, target_prime_y_task = data["target_prime_X"], data["target_prime_y_task"]
-    feature_extractor, task_classifier_dim1, task_classifier_dim2  = (
+    feature_extractor, task_classifier  = (
         network["feature_extractor"],
-        network["task_classifier_dim1"],
-        network["task_classifier_dim2"],
+        network["task_classifier"],
     )
     criterion = network["criterion"]
-    feature_optimizer, task_optimizer_dim1, task_optimizer_dim2 = (
+    feature_optimizer, task_optimizer = (
         network["feature_optimizer"],
-        network["task_optimizer_dim1"],
-        network["task_optimizer_dim2"],
+        network["task_optimizer"],
     )
     config = {
+        "alpha": 1,
         "num_epochs": 1000,
         "device": utils.DEVICE,
         "do_early_stop": False,
         "do_plot": False,
     }
     config.update(kwargs)
+    alpha = config["alpha"]
     num_epochs, device = config["num_epochs"], config["device"]
     do_early_stop = config["do_early_stop"]
     do_plot = config["do_plot"]
@@ -46,12 +46,11 @@ def fit(data, network, **kwargs):
     early_stopping = EarlyStopping()
     num_epochs = torch.tensor(num_epochs, dtype=torch.int32).to(device)
     for epoch in tqdm(range(1, num_epochs + 1)):
-        task_classifier_dim1.train()
-        task_classifier_dim2.train()
+        task_classifier.train()
         feature_extractor.train()
 
         for (source_X_batch, source_Y_batch), (target_X_batch, _), (target_prime_X_batch, ) in zip(source_loader, target_loader, target_prime_loader):
-            if task_classifier_dim1.output_size == 1:
+            if task_classifier.output_size == 1:
                 source_y_task_batch = source_Y_batch[:, utils.COL_IDX_TASK]
                 source_y_task_batch = source_y_task_batch.to(torch.float32)
             else:
@@ -59,9 +58,53 @@ def fit(data, network, **kwargs):
                 source_y_task_batch = source_y_task_batch.to(torch.long)
             
             # 1. Forward
+            source_X_batch = feature_extractor(source_X_batch)
+            target_X_batch = feature_extractor(target_X_batch)
+            target_prime_X_batch = feature_extractor(target_prime_X_batch)
+            source_out = task_classifier(source_X_batch)
+            target_out = task_classifier(target_X_batch)
+            target_prime_out = task_classifier(target_prime_X_batch)
+            ## 1.1 Task Loss
+            if task_classifier.output_size == 1:
+                source_preds = torch.sigmoid(source_out).reshape(-1)
+                loss_task = criterion(source_preds, source_y_task_batch)
+            else:
+                source_preds = torch.softmax(source_out, dim=1)
+                loss_task = criterion(source_preds, source_y_task_batch)
+                loss_task = loss_task.mean()
+
+            ## 1.2 CoRAL Loss
+            cov_mat_source, cov_mat_target = get_covariance_matrix(source_out, target_out)
+            _, cov_mat_target_prime = get_covariance_matrix(source_out, target_prime_out)
+
+            k = source_out.shape[1]
+            loss_coral = get_MSE(cov_mat_source, cov_mat_target) * (1 / (4 * k ** 2))
+            loss_coral += get_MSE(cov_mat_target, cov_mat_target_prime) * (1 / (4 * k ** 2))
+
+            loss_corals.append(loss_coral.item())
+            loss_tasks.append(loss_task.item())
+            loss = loss_task + loss_coral * alpha
 
             # 2. Backward
+            task_optimizer.zero_grad()
+            feature_optimizer.zero_grad()
+            loss.backward()
 
             # 3. Update Params
-        
+            task_optimizer.step()
+            feature_optimizer.step()
         # 4. Eval
+        with torch.no_grad():
+            feature_extractor.eval()
+            task_classifier.eval()
+            target_prime_out = task_classifier.predict(feature_extractor(target_prime_X))
+            acc = sum(target_out == target_prime_y_task) / len(target_prime_y_task)
+            if epoch % 10 == 0:
+                print(f"Epoch: {epoch}, Loss Coral: {loss_coral}, Loss Task: {loss_task}, Acc: {acc}")
+            early_stopping(acc.item())
+            loss_task_evals.append(acc.item())
+        if early_stopping.early_stop & do_early_stop:
+            break
+    if do_plot:
+        plot_coral_loss(loss_corals, loss_tasks, loss_task_evals)
+    return feature_extractor, task_classifier, None
--- algo/coral_algo.py
+++ algo/coral_algo.py
@@ -141,11 +141,11 @@ def fit(data, network, **kwargs):
             break
 
     if do_plot:
-        _plot_coral_loss(loss_corals, loss_tasks, loss_evals)
+        plot_coral_loss(loss_corals, loss_tasks, loss_evals)
     return feature_extractor, task_classifier, None
 
 
-def _plot_coral_loss(loss_corals, loss_tasks, loss_evals):
+def plot_coral_loss(loss_corals, loss_tasks, loss_evals):
     plt.plot(loss_corals, label="loss coral")
     plt.plot(loss_tasks, label="loss task")
     plt.xlabel("batch")
