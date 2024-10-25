90c4f2bf5479575582f585a2f09eaa0588662b6c
oh-yu <perusyanekolovve0320@gmail.com>
2024-10-25 14:22:59 +0900

    implemented # 0. Data

--- algo/jdot_algo.py
+++ algo/jdot_algo.py
@@ -79,6 +79,23 @@ def fit(data, network, **kwargs):
                 changed_lrs=changed_lrs,
             )
         # batch_training
+        for (source_X_batch, source_Y_batch), (target_X_batch, _) in zip(
+            source_loader, target_loader
+        ):
+            # 0. Data
+            if task_classifier.output_size == 1:
+                source_y_task_batch = source_Y_batch[:, utils.COL_IDX_TASK] > 0.5
+                source_y_task_batch = source_y_task_batch.to(torch.float32)
+            else:
+                if is_psuedo_weights:
+                    output_size = source_Y_batch[:, :-1].shape[1]
+                    source_y_task_batch = source_Y_batch[:, :output_size]
+                    source_y_task_batch = torch.argmax(source_y_task_batch, dim=1)
+                    source_y_task_batch = source_y_task_batch.to(torch.long)
+                else:
+                    source_y_task_batch = source_Y_batch[:, utils.COL_IDX_TASK]
+                    source_y_task_batch = source_y_task_batch.to(torch.long)
+
 
 
 
