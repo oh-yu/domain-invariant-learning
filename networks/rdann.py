64bf4dadbe29be581701e6a1712e59497a834c62
oh-yu <perusyanekolovve0320@gmail.com>
2025-01-04 13:07:21 +0900

    use base

--- networks/rdann.py
+++ networks/rdann.py
@@ -3,67 +3,27 @@ from rnn import ManyToOneRNN
 from torch import nn, optim
 
 from networks.mlp_decoder_three_layers import ThreeLayersDecoder
-
 from ..algo import dann_algo
+from .base import DannsBase
+from ..utils import utils
 
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
-class Rdann:
+class Rdann(DannsBase):
     """
     R-DANN model https://browse.arxiv.org/pdf/2005.10996.pdf
     """
 
     def __init__(
-        self,
-        input_size: int,
-        hidden_size: int,
-        lr: float,
-        num_epochs: int,
-        num_layers: int = 3,
-        num_domains: int = 1,
-        num_classes: int = 1,
+        self, experiment: str
     ) -> None:
-        self.feature_extractor = ManyToOneRNN(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers).to(
-            DEVICE
-        )
-        self.domain_classifier = ThreeLayersDecoder(input_size=hidden_size, output_size=num_domains).to(DEVICE)
-        self.task_classifier = ThreeLayersDecoder(input_size=hidden_size, output_size=num_classes).to(DEVICE)
-        self.criterion = nn.BCELoss()
+        if experiment == "HHAR":
+            self.feature_extractor = ManyToOneRNN(input_size=6, hidden_size=128, num_layers=3).to(
+                utils.DEVICE
+            )
+            self.domain_classifier = ThreeLayersDecoder(input_size=128, output_size=1).to(utils.DEVICE)
+            self.task_classifier = ThreeLayersDecoder(input_size=128, output_size=10).to(utils.DEVICE)
+            self.criterion = nn.BCELoss()
 
-        self.feature_optimizer = optim.Adam(self.feature_extractor.parameters(), lr=lr)
-        self.domain_optimizer = optim.Adam(self.domain_classifier.parameters(), lr=lr)
-        self.task_optimizer = optim.Adam(self.task_classifier.parameters(), lr=lr)
-        self.num_epochs = num_epochs
-
-    def fit(
-        self,
-        source_loader: torch.utils.data.dataloader.DataLoader,
-        target_loader: torch.utils.data.dataloader.DataLoader,
-        test_target_X: torch.Tensor,
-        test_target_y_task: torch.Tensor,
-    ) -> None:
-        data = {
-            "source_loader": source_loader,
-            "target_loader": target_loader,
-            "target_X": test_target_X,
-            "target_y_task": test_target_y_task,
-        }
-        network = {
-            "feature_extractor": self.feature_extractor,
-            "domain_classifier": self.domain_classifier,
-            "task_classifier": self.task_classifier,
-            "criterion": self.criterion,
-            "feature_optimizer": self.feature_optimizer,
-            "domain_optimizer": self.domain_optimizer,
-            "task_optimizer": self.task_optimizer,
-        }
-        config = {
-            "num_epochs": self.num_epochs,
-        }
-        self.feature_extractor, self.task_classifier, _ = dann_algo.fit(data, network, **config)
-
-    def predict(self, x: torch.Tensor) -> torch.Tensor:
-        pred_y_task = self.task_classifier(self.feature_extractor(x))
-        pred_y_task = torch.sigmoid(pred_y_task).reshape(-1)
-        return pred_y_task
+            self.feature_optimizer = optim.Adam(self.feature_extractor.parameters(), lr=1e-3)
+            self.domain_optimizer = optim.Adam(self.domain_classifier.parameters(), lr=1e-3)
+            self.task_optimizer = optim.Adam(self.task_classifier.parameters(), lr=1e-3)
+            self.num_epochs = 100
