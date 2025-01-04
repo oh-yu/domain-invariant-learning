2bd0a517c92682275cc340f952f0c3fcf8159794
oh-yu <perusyanekolovve0320@gmail.com>
2025-01-04 11:48:12 +0900

    refactor: use base

--- networks/codats_fc.py
+++ networks/codats_fc.py
@@ -5,11 +5,12 @@ from ..algo import supervised_algo
 from .conv1d_three_layers import Conv1dThreeLayers
 from .conv1d_two_layers import Conv1dTwoLayers
 from .mlp_decoder_three_layers import ThreeLayersDecoder
+from .base import SupervisedBase
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-class CoDATS_F_C(nn.Module):
+class CoDATS_F_C(nn.Module, SupervisedBase):
     def __init__(self, experiment: str):
         super().__init__()
         assert experiment in ["ECOdataset", "ECOdataset_synthetic", "HHAR"]
@@ -28,34 +29,3 @@ class CoDATS_F_C(nn.Module):
             self.optimizer = optim.Adam(list(self.conv1d.parameters()) + list(self.decoder.parameters()), lr=1e-4)
             self.criterion = nn.CrossEntropyLoss()
             self.num_epochs = 300
-
-    def fit_without_adapt(self, source_loader):
-        data = {"loader": source_loader}
-        network = {
-            "decoder": self.decoder,
-            "encoder": self.conv1d,
-            "optimizer": self.optimizer,
-            "criterion": self.criterion,
-        }
-        config = {"use_source_loader": True, "num_epochs": self.num_epochs}
-        supervised_algo.fit(data, network, **config)
-
-    def fit_on_target(self, target_prime_loader):
-        data = {"loader": target_prime_loader}
-        network = {
-            "decoder": self.decoder,
-            "encoder": self.conv1d,
-            "optimizer": self.optimizer,
-            "criterion": self.criterion,
-        }
-        config = {"use_source_loader": False, "num_epochs": self.num_epochs}
-        supervised_algo.fit(data, network, **config)
-
-    def forward(self, x):
-        return self.decoder(self.conv1d(x))
-
-    def predict(self, x):
-        return self.decoder.predict(self.conv1d(x))
-
-    def predict_proba(self, x):
-        return self.decoder.predict_proba(self.conv1d(x))
--- networks/dann_Fc.py
+++ networks/dann_Fc.py
@@ -4,48 +4,15 @@ from torch import nn, optim
 from ..algo import supervised_algo
 from .conv2d import Conv2d
 from .mlp_decoder_three_layers import ThreeLayersDecoder
+from .base import SupervisedBase
 
 
-class Dann_F_C(nn.Module):
+class Dann_F_C(nn.Module, SupervisedBase):
     def __init__(self):
         super().__init__()
         self.device = torch.device("cpu")
-        self.conv2d = Conv2d().to(self.device)
+        self.encoder = Conv2d().to(self.device)
         self.decoder = ThreeLayersDecoder(input_size=1152, output_size=10, fc1_size=3072, fc2_size=2048).to(self.device)
         self.optimizer = optim.Adam(list(self.conv2d.parameters()) + list(self.decoder.parameters()), lr=1e-4)
         self.criterion = nn.CrossEntropyLoss()
         self.num_epochs = 10
-
-    def fit_without_adapt(self, source_loader):
-        data = {"loader": source_loader}
-        network = {
-            "decoder": self.decoder,
-            "encoder": self.conv2d,
-            "optimizer": self.optimizer,
-            "criterion": self.criterion,
-        }
-        config = {"use_source_loader": True, "num_epochs": self.num_epochs}
-        supervised_algo.fit(data, network, **config)
-
-    def fit_on_target(self, train_target_prime_loader):
-        data = {"loader": train_target_prime_loader}
-        network = {
-            "decoder": self.decoder,
-            "encoder": self.conv2d,
-            "optimizer": self.optimizer,
-            "criterion": self.criterion,
-        }
-        config = {
-            "use_source_loader": False,
-            "num_epochs": self.num_epochs,
-        }
-        supervised_algo.fit(data, network, **config)
-
-    def forward(self, x):
-        return self.decoder(self.conv2d(x))
-
-    def predict(self, x):
-        return self.decoder.predict(self.conv2d(x))
-
-    def predict_proba(self, x):
-        return self.decoder.predict_proba(self.conv2d(x))
